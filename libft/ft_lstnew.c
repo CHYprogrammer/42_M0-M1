f277f5a04f338d8095982789344f00a2f1083e33
Heyu Chong <heychong@c5r4s9.42tokyo.jp>
2025-12-02 22:00:41 +0900

    libft ft_lstnew minor change

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heychong <heychong@student.42tokyo.jp      +#+  +:+       +#+        */
+/*   By: heychong <heychong@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 18:24:19 by heychong          #+#    #+#             */
-/*   Updated: 2025/10/28 19:27:31 by heychong         ###   ########.fr       */
+/*   Updated: 2025/12/02 20:51:27 by heychong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ t_list	*ft_lstnew(void *input)
 {
 	t_list	*res;
 
-	res = malloc(sizeof(void *) + sizeof(t_list *));
+	res = malloc(sizeof(t_list));
+	if (!res)
+		return (NULL);
 	res->content = input;
 	res->next = NULL;
 	return (res);
