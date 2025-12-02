954629f39229adaaa19bbf0592e21f7fd6742c09
Chong Heyu <heyu@Chongnonotobukkukonpyuta.local>
2025-12-02 15:16:42 +0900

    printf logic

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heychong <heychong@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: heyu <heyu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 12:42:48 by heyu              #+#    #+#             */
-/*   Updated: 2025/11/30 19:33:55 by heychong         ###   ########.fr       */
+/*   Updated: 2025/12/01 15:09:23 by heyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,37 @@
 int	print_error(char *error_message)
 {
 	write(2, error_message, ft_strlen(error_message));
-	return (1);
+	return (-1);
 }
+
+int	print_format(const char *var_str, ...);
+
 
 int	ft_printf(const char *format, ...)
 {
-	char	*find;
-	char	*str;
-	char	*trimmed;
+	size_t	i;
 	size_t	len;
-	char	error_message[128];
+	char	*var_str;
+	char	format_len;
 
-	if (!format)
-		return (1);
-	error_message = "error";
-	find = ft_strdup(ft_strchr(format, '%'));
-	trimmed = ft_strdup(ft_strtrim(format, find));
-	if (trimmed)
-		write(1, trimmed, ft_strlen(trimmed));
-	if (find)
+	i = 0;
+	len = 0;
+	while (format[i])
 	{
-		va_start()
-		if (find[1] == 'c')
-
-		else if (find[1] == 's')
-		else if (find[1] == 'p')
-		else if (find[1] == 'd')
-		else if (find[1] == 'i')
-		else if (find[1] == 'u')
-		else if (find[1] == 'x')
-		else if (find[1] == 'X')
-		else if (find[1] == '%')
-			write(1, "%%", 1);
-		else
-			print_error("wrong format");
+		if (format[i] == '%')
+		{
+			format_len = printf_format(var_str, ...);
+			if (format_len < 0)
+				return (print_error("wrong format"));
+			else
+				len += format_len;
+			i += 2;
+			continue ;
+		}
+		if (write(1, &format[i], 1) == -1)
+			return (-1);
+		len++;
+		i++;
 	}
-	else
-		write(1, format, len);
+	return (len);
 }
