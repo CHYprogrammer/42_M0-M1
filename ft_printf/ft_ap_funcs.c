8482efe75d93163f066974820366629c9aa84bc9
HEYU CHONG <heychong@c3r3s7.42tokyo.jp>
2025-12-17 20:25:44 +0900

    まーじで覚えてろよ

--- ft_printf/ft_ap_funcs.c
+++ ft_printf/ft_ap_funcs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ap_funcs.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heyu <heyu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: heychong <heychong@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 21:36:54 by heychong          #+#    #+#             */
-/*   Updated: 2025/12/08 17:13:03 by heyu             ###   ########.fr       */
+/*   Updated: 2025/12/17 14:07:24 by heychong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,10 @@ int	print_str_ap(char *str)
 int	print_address_ap(void *ptr)
 {
 	unsigned int	tmp;
-	unsigned int	digit;
 	char			*str;
 
 	tmp = (unsigned int)ptr;
-	digit = count_hex_digit(tmp);
-	str = ft_itox(digit, tmp, "0123456789abcdef");
+	str = ft_itox(count_hex_digit(tmp), tmp, "0123456789abcdef");
 	ret = print_str_ap(str);
 	if (str)
 		free(str);
--- ft_printf/ft_ap_funcs2.c
+++ ft_printf/ft_ap_funcs2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ap_funcs2.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heyu <heyu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: heychong <heychong@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 15:51:43 by heyu              #+#    #+#             */
-/*   Updated: 2025/12/08 17:26:16 by heyu             ###   ########.fr       */
+/*   Updated: 2025/12/17 14:06:14 by heychong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,19 @@ char	*ft_itox(unsigned int digit, unsigned int unbr, char *base)
 	char			*hex;
 	unsigned int	i;
 
-	hex = malloc(digit + 1);
+	hex = malloc(digit + 2 + 1);
 	if (!hex)
 		return (NULL);
 	i = 1;
+	hex[0] = '0';
+	hex[1] = 'x';
 	while (unbr)
 	{
-		hex[digit - i] = base[unbr % 16];
+		hex[digit - i + 2] = base[unbr % 16];
 		unbr /= 16;
 		i++;
 	}
-	hex[digit] = '\0';
+	hex[digit + 2] = '\0';
 	return (hex);
 }
 
